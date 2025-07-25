f135181578361a3d034d120136a1a89f60a094e9
Teejayskillz <Teejayadewale87@gmail.com>
2025-07-25 04:12:27 +0100

    added registration

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here
+
+class userRegistration(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
