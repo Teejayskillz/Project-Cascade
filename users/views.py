6c7af08665cdf64235c2f4959de821b5a20a1a9e
Teejayskillz <Teejayadewale87@gmail.com>
2025-07-25 06:12:53 +0100

    added login,logout

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .forms import UserRegistrationForm 
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate , logout
+from django.shortcuts import redirect
+
 
 def register_user(request):
     if request.method == 'POST':
@@ -19,4 +21,21 @@ def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-    return render(request, 'users/login.html')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a home page or dashboard after login
+            else:
+                form.add_error(None, "Invalid username or password")
+        return render(request, 'users/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_user(request):
+    # Placeholder for logout logic
+
+    logout(request)
+    return redirect('home')  # Redirect to login page after logout    
