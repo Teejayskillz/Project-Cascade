bc17b7967b5ce14d1a967839be9a533687634889
Teejayskillz <Teejayadewale87@gmail.com>
2025-08-27 00:08:24 +0100

    serving css admin added ststic file storage for white moise

--- jaraflix/settings.py
+++ jaraflix/settings.py
@@ -68,9 +68,8 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
