f2185947c6e35c9903a904c1fe84732bb8f27408
Teejayskillz <Teejayadewale87@gmail.com>
2025-08-28 11:44:20 +0100

    added notification page

--- user_notifications/urls.py
+++ user_notifications/urls.py
@@ -8,4 +8,6 @@ app_name = "notifications"
 urlpatterns = [
     path("notifications/json/", views.get_unread_notifications, name="unread_notifications_json"),
     path('unread-notifications/', views.get_unread_notifications, name='get_unread_notifications'),
+    path('unread-count/', views.get_unread_notifications, name='unread_count'),
+    path('', views.NotificationListView.as_view(), name='notification_list'),
 ]
--- user_notifications/views.py
+++ user_notifications/views.py
@@ -1,6 +1,27 @@
 # notifications/views.py
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.urls import reverse
+
+from notifications.models import Notification
+
+
+class NotificationListView(LoginRequiredMixin, ListView):
+    model = Notification
+    template_name = 'notifications/notification_list.html'
+    context_object_name = 'notifications'
+    paginate_by = 20
+
+    def get_queryset(self):
+        return Notification.objects.filter(recipient=self.request.user).order_by('-timestamp')
+    
+    def get(self, request, *args, **kwargs):
+        request.user.notifications.all().mark_all_as_read()
+        return super().get(request, *args, **kwargs)
+    
+
 
 @login_required
 def get_unread_notifications(request):
@@ -11,8 +32,8 @@ def get_unread_notifications(request):
             "actor": str(n.actor),
             "verb": n.verb,
             "timestamp": n.timestamp.strftime("%H:%M"),
+            "actor_url": reverse('users:user_profile', kwargs={'username': n.actor.username}) # Add this line
         }
         for n in notifications
     ]
     return JsonResponse({"count": notifications.count(), "notifications": data})
-
